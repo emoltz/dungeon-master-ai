3b9b33bcb8faa8a6504765121f23fd23a72f1aef
Ethan Shafran Moltz <emoltz@carnegielearning.com>
2024-03-26 10:23:56 -0400

    Add base URL and routes for DND API service

--- lib/dndApiService.ts
+++ lib/dndApiService.ts
@@ -1,7 +1,14 @@
 import { GetAllResourcesList } from "./types";
 
+export const BASE_URL = "https://www.dnd5eapi.co";
+export const routes = {
+    all_classes: "/api/classes/",
+    all_races: "/api/races/",
+
+}
+
 export async function getClasses(): Promise<GetAllResourcesList> {
-    const res = await fetch("https://www.dnd5eapi.co/api/classes/");
+    const res = await fetch(BASE_URL + routes.all_classes);
 
     if (!res.ok) {
         throw new Error("Failed to fetch");
@@ -11,7 +18,7 @@ export async function getClasses(): Promise<GetAllResourcesList> {
 }
 
 export async function getRaces(): Promise<GetAllResourcesList> {
-    const res = await fetch("https://www.dnd5eapi.co/api/races/");
+    const res = await fetch(BASE_URL + routes.all_races);
     if (!res.ok) {
         throw new Error("Failed to fetch");
     }
