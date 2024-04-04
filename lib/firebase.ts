6948fa4ef886965be15ce4674f1496e1e6e41f08
Ethan Shafran Moltz <33405530+emoltz@users.noreply.github.com>
2024-04-03 20:12:31 -0400

    Add Firebase Auth and User type

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,13 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+import {Auth, getAuth, User} from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCwO3fZwEgcAY8duJfmd4ZBJMzXJ-3r7NU",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "ai-dm-4fc60.firebaseapp.com",
   projectId: "ai-dm-4fc60",
   storageBucket: "ai-dm-4fc60.appspot.com",
--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,8 @@
+export type User = {
+  id: number;
+  name: string;
+};
+
 export type GetAllResourcesList = {
   count: number;
   results: IndexItem[];
